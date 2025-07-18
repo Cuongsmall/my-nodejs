4a79d352ee8bfc91b90ce6f73e8ba81a3ae5e0fa
Cuongsmall <cuongnmps38652@gmail.com>
2025-07-18 16:27:06 +0700

    get use to express

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Hello world")
+const express = require('express')
+
+const app = express();
+const PORT = 8080;
+app.get("/", (req, res) => {
+    res.send("Hello Word")
+})
+
+app.get("/cuong", (req, res) => {
+    res.send("Hello cuong")
+})
+app.listen(PORT, () => {
+    console.log(`ok ${PORT}`);
+})
