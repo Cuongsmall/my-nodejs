16642f6fdc36d45d2e5693b6aea059fb2c44f43d
Cuongsmall <cuongnmps38652@gmail.com>
2025-08-10 22:56:45 +0700

    check code

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
 //config view engine
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
